2dad32e9b8b3693cc6c1bef7ee07487ad4e711a2
Annika Gadman <annika.gadman@gmail.com>
2025-05-02 09:44:08 -0500

    api för register och login skapat

---
+++ routes/authRoutes.js
@@ -0,0 +1,48 @@
+/**
+ * Rotes for auth
+ */
+
+const express = require('express');
+const router = express.Router();
+
+// Add a new user
+router.post('/register', async (req, res) => {    
+    try {
+        const { username, password } = req.body;
+
+        // Validate input
+        if (!username || !password) {
+            return res.status(400).json({ error: 'Username and password are required' });
+        }
+        // Correct save user
+        res.status(201).json({ message: 'User created' });
+
+    } catch (error) {
+        res.status(500).json({ error: 'Server error' });
+    };
+});
+
+
+// Login user
+router.post('/login', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+
+        // Validate input
+        if (!username || !password) {
+            return res.status(400).json({ error: 'Username and password are required' });
+        }
+
+        // Check credentials
+        if (username === 'annika' && password === 'password') {
+            res.status(200).json({ message: 'Login successful' });
+        } else {
+            res.status(401).json({ error: 'Invalid credentials' });
+        }
+
+    } catch (error) {
+        res.status(500).json({ error: 'Server error' });
+    }
+});
+
+module.exports = router;
