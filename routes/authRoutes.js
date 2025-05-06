ce714bf61cc665cc58df7ca1844f26ce784db3fa
Annika Gadman <annika.gadman@gmail.com>
2025-05-06 10:07:01 -0500

    skapat JWT

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,6 +5,7 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 // Connect to Mongodb
@@ -60,7 +61,14 @@ router.post('/login', async (req, res) => {
         if (!isPasswordMatch) {
             return res.status(401).json({ error: 'Incorrect username/password' });
         } else {
-            res.status(200).json({ message: 'Login successful' });
+            // Create JWT   
+            const payload = { username: username};
+            const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, {expiresIn: '1h'});
+            const response = {
+                message: 'User logged in!',
+                token: token,
+            };
+            res.status(200).json({ response });
         }
 
     } catch (error) {
