ff3a8d5301ac350f591e2a5e9a932124cc6749c1
Annika Gadman <annika.gadman@gmail.com>
2025-05-05 22:56:33 -0500

    registrering och inlogg skapat med MongoDb

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,6 +4,20 @@
 
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+// Connect to Mongodb
+mongoose.set('strictQuery', false);
+mongoose.connect(process.env.DATABASE).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+}
+);
+
+// User model
+const User = require('../models/User');
 
 // Add a new user
 router.post('/register', async (req, res) => {    
@@ -15,6 +29,8 @@ router.post('/register', async (req, res) => {
             return res.status(400).json({ error: 'Username and password are required' });
         }
         // Correct save user
+        const user = new User({ username, password });
+        await user.save();
         res.status(201).json({ message: 'User created' });
 
     } catch (error) {
@@ -34,10 +50,17 @@ router.post('/login', async (req, res) => {
         }
 
         // Check credentials
-        if (username === 'annika' && password === 'password') {
-            res.status(200).json({ message: 'Login successful' });
+        //Does user exist?
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(401).json({ error: 'Incorrect username/password' });
+        }
+        // Check password
+        const isPasswordMatch = await user.comparePassword(password);
+        if (!isPasswordMatch) {
+            return res.status(401).json({ error: 'Incorrect username/password' });
         } else {
-            res.status(401).json({ error: 'Invalid credentials' });
+            res.status(200).json({ message: 'Login successful' });
         }
 
     } catch (error) {
